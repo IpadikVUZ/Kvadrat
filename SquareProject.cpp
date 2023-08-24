308a71d9fa02c46bfe7795c903c8d18dddd681a6
IpadikVUZ <142499283+IpadikVUZ@users.noreply.github.com>
2023-08-24 22:02:28 +0300

    Убрал пробел

--- SquareProject.cpp
+++ SquareProject.cpp
@@ -1,7 +1,7 @@
 #include "SquareSolver.h"
 
 int main(){
-    int N = 0; // Переменная, отвечающая за номер сообщения
+int N = 0; // Переменная, отвечающая за номер сообщения
 double a = 0, b = 0, c = 0, x1 = 0, x2 = 0; // ax^2+bx+c=0    input (&a, &b, &c);
 testsquaresolver();
 input(&a,&b,&c);
